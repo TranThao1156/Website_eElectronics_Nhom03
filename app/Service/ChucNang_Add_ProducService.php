b5699d92cf5875022b8f0b0cf8047808972dc698
KhanhTuanNguyen <0306231114@caothang.edu.vn>
2025-10-11 20:08:24 +0700

    Hiển thị nội dung của nhà cung cấp và danh mục trên thanh cuôn

--- app/Service/ChucNang_Add_ProducService.php
+++ app/Service/ChucNang_Add_ProducService.php
@@ -3,11 +3,16 @@ namespace App\Service;
 
 use Illuminate\Support\Facades\DB;
 
-class DanhMucService
+class ChucNang_Add_ProducService
 {
     //Thảo - Lấy tất cả danh mục
     public function getAll()
     {
         return DB::select("SELECT * FROM danhmuc WHERE TrangThai = 1");
+    }
+    //Khanh - Lấy tất cả nhà cung cấp
+    public function getNhaCungCap()
+    {
+        return DB::select("SELECT * FROM nhacungcap WHERE TrangThai = 1");
     }
 }
