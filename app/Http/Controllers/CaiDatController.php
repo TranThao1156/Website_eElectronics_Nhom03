1121c1ee623267c99dc79ab0dc0b51c75bc60f55
KhanhTuanNguyen <0306231114@caothang.edu.vn>
2025-10-28 23:43:54 +0700

    CaiDat_migration

--- app/Http/Controllers/CaiDatController.php
+++ app/Http/Controllers/CaiDatController.php
@@ -1,7 +1,19 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Models\CaiDat;
+use Illuminate\Support\Facades\View;
 
 class CaiDatController extends Controller
 {
-    
+    // Hàm này bạn có thể gọi trong AppServiceProvider để chia sẻ toàn cục
+    public static function boot()
+    {
+        $caidat = CaiDat::getThongTin();
+        $socialLinks = CaiDat::getSocialLinks();
+
+        View::share('caidat', $caidat);
+        View::share('socialLinks', $socialLinks);
+    }
 }
--- app/Models/CaiDat.php
+++ app/Models/CaiDat.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class CaiDat extends Model
 {
-    
+    protected $table = 'caidat'; // Tên bảng
+    protected $fillable = [
+        'TenWebsite', 'MoTa', 'Facebook', 'Twitter', 'Youtube',
+        'Linkedin', 'Pinterest', 'Copyright'
+    ];
+
+    // Lấy bản ghi đầu tiên
+    public static function getThongTin()
+    {
+        return self::first();
+    }
+
+    // Lấy riêng link mạng xã hội
+    public static function getSocialLinks()
+    {
+        return self::select('Facebook', 'Twitter', 'Youtube', 'Linkedin', 'Pinterest')->first();
+    }
 }
